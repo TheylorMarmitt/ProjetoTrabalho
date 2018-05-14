22d947d870616053f5776d3051ab5fa84423ab85
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-05-14 19:49:23 -0300

    Adicionando DAO vendedor

--- trabalhoPA/src/principal/VendedorController.java
+++ trabalhoPA/src/principal/VendedorController.java
@@ -15,6 +15,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import principal.dao.FilialArquivo;
 import principal.dao.FilialDAO;
+import principal.dao.VendedorArquivo;
+import principal.dao.VendedorDAO;
 import principal.model.Filial;
 import principal.model.Vendedor;
 
@@ -57,11 +59,8 @@ public class VendedorController {
     @FXML
     private ComboBox<Filial> cbFilial;
     
-    private Vendedor vendedor;
     
-    private Filial filial;
-    
-    private FilialDAO filialDAO = new FilialArquivo(); 
+    private VendedorDAO vendedorDAO = new VendedorArquivo(); 
     
     private boolean editando;
     
@@ -75,36 +74,27 @@ public class VendedorController {
 	}
     
 	private void populaCombo(){
-		for(Filial filial: filialDAO.listar()){
-			cbFilial.getItems().add(filial);
+		for(Vendedor vendedor: vendedorDAO.listar()){
+			cbFilial.getItems().add(vendedor);
 		}
 	}
 
     @FXML
     void adicionarVendedor(ActionEvent event) {
     		populaVendedor();
-    		if(editando) {
-    			// pega a lista vendedor da filial
-    			List<Vendedor> dados = filial.getVendedores();
-    			for (Vendedor vendedores : dados) {
-    				if (vendedores.equals(dados)) {
-    					dados.remove(vendedores);
-    					dados.add(vendedor);
-    					break;
-    				}
-    			}
-    			filial.setVendedores(dados);
-    			// exclui e filial atual do arquivo
-    			filialDAO.excluir(filial);
-    			// add a nova filial no arquivo
-    			filialDAO.inserir(filial);
-    		}else {
-    			List<Vendedor> dados = filial.getVendedores();
-    			dados.add(vendedor);
-    			filial.setVendedores(dados);
-    			filialDAO.excluir(filial);
-    			filialDAO.inserir(filial);
-    		}
+    		if (editando) {
+				vendedorDAO.alterar(vendedor);
+			} else {
+				vendedorDAO.inserir(vendedor);
+			}
+			
+			
+			populaVendedor();
+	
+			if (!editando) {
+				tbCarro.getItems().add(carro);
+			}
+			novoCarro();
     }
     
     @FXML
@@ -158,7 +148,7 @@ public class VendedorController {
 		tfCPF.clear();
 		tfEmail.clear();
 		dtNascimento.setValue(null);
-		//tblVendedor.setItems(FXCollections.observableArrayList(filial.getVendedores()));
+		tblVendedor.setItems(FXCollections.observableArrayList(vendedorDAO.);
 		
     }
 	
