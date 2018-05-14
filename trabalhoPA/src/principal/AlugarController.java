f8a88fc6b5442843c2531e629d96881573cd5f93
Everson <everson.signori@unoesc.com.br>
2018-05-14 20:27:58 -0300

    Mudança

---
+++ trabalhoPA/src/principal/AlugarController.java
@@ -0,0 +1,37 @@
+package principal;
+import com.sun.glass.events.MouseEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class AlugarController {
+
+    @FXML
+    private ComboBox<?> cbcarros;
+
+    @FXML
+    private ComboBox<?> cbVendendor;
+
+    @FXML
+    private ComboBox<?> cbFillal;
+
+    @FXML
+    private TableView<?> tblVendedor;
+
+    @FXML
+    private TableColumn<?, ?> tbcCodigo;
+
+    @FXML
+    private TableColumn<?, ?> tbcNome;
+
+    @FXML
+    private Button btnAlugar;
+
+    @FXML
+    void selecionaVendedor(MouseEvent event) {
+
+    }
+
+}
